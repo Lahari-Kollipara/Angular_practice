ffca2f8d18bcf65402b1e6995e52e83249e50541
lahari-kollipara <lakshmi.k.dea@gmail.com>
2025-07-12 10:32:33 -0400

    july 10, 11 commit

---
+++ july_11/book.js
@@ -0,0 +1,10 @@
+class Book{
+    constructor(id, name, author){
+        this.id=id;
+        this.name=name;
+        this.author=author;
+    }
+}
+
+const book1=new Book(2123, 'harryPotter', 'Rowling');
+console.log(book1);
